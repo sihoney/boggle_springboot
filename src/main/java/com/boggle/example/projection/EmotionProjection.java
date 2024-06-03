f2a4fd495c4d0dcceb62b262eac0bb8853944e67
sihoney <hwangheewon@naver.com>
2024-06-03 20:37:30 +0900

    Update .gitignore and remove ignored files from tracking

--- src/main/java/com/boggle/example/projection/EmotionProjection.java
+++
@@ -1,7 +0,0 @@
-package com.boggle.example.projection;
-
-public interface EmotionProjection {
-	Long getEmotionId();
-	String getEmotionName();
-	Long getTotalCount();
-}
--- src/main/java/com/boggle/example/projection/ReviewCountProjection.java
+++
@@ -1,8 +0,0 @@
-package com.boggle.example.projection;
-
-import java.time.LocalDateTime;
-
-public interface ReviewCountProjection {
-	String getPeriod();
-	Long getReviewCount();
-}
